179648f6e34bd0c9cf620b2ec0397d707e0ca6dd
covidcodefest <67840035+covidcodefest@users.noreply.github.com>
2020-07-06 19:40:02 +0530

    Add files via upload
    Solution for hard sample problem

---
+++ Sample_problem_hard.py
@@ -0,0 +1,22 @@
+def revDigits(num) : # function for reversing digits
+    rev_num = 0;
+    while(num > 0):
+        rev_num = rev_num*10 + num%10
+        num = num//10 
+    return rev_num
+
+biggest=0
+num1=0
+num2=0
+for i in range(100,1000):
+    for j in range(100,1000):
+        product=i*j
+        if product==revDigits(product): # checking palindrome condition
+            if product>biggest:
+                biggest=product
+                num1=i
+                num2=j
+                
+print("Answer is : "+str(biggest))  # 906609
+
+print("From numbers : ", str(num1),str(num2))  # 913, 993
