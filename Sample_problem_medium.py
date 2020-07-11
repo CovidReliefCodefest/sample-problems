4b2e9e7edaae0c1416ce4e4af3d3c011892a7598
CovidReliefCodefest <67840035+CovidReliefCodefest@users.noreply.github.com>
2020-07-11 19:44:12 +0530

    Update Sample_problem_medium.py

--- Sample_problem_medium.py
+++ Sample_problem_medium.py
@@ -1,21 +1,19 @@
-def revWord(word):
+def revWord(word): # defining a function to reverse the word
     newword=""
     for i in range(0,len(word)):
-        newword+=word[len(word)-i-1]
+        newword+=word[len(word)-(i+1)] 
     return newword
-    
-print(revWord("buddy"))
 
 sentence="My name is Aditya."
 revSent=""
-tempWord=""
-for j in sentence:
-    if j==" " or j==".":
-        revSent+=revWord(tempWord)+j
+tempWord="" # variable that stores the word until a space or full stop comes
+for j in sentence: # traversing through the sentence
+    if j==" " or j==".": # helps us sepraate each word individually and reverse it
+        revSent+=revWord(tempWord)+j # calling  the revWord function to reverse the word
         tempWord=""
         
     
     else:
-        tempWord+=j
+        tempWord+=j # adding on to the temporary word
         
 print(revSent)
